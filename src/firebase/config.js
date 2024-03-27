ca2584ae348395a19665cccbede46b71bf662c28
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-03-27 14:18:39 -0300

    conexao firebase

---
+++ src/firebase/config.js
@@ -0,0 +1,16 @@
+import { initializeApp } from "firebase/app";
+import {getFirestore}  from 'firebase/firestore';
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAWDiCPDD7_pdql9r9kxhmSKLGHnMXyDeY",
+  authDomain: "blogtravel-3d91a.firebaseapp.com",
+  projectId: "blogtravel-3d91a",
+  storageBucket: "blogtravel-3d91a.appspot.com",
+  messagingSenderId: "749577791433",
+  appId: "1:749577791433:web:1b00022486f3160e296a18"
+};
+
+const db = getFirestore(app);
+const app  = initializeApp(firebaseConfig);
+
+export{db};
