dbd045c1a620103966fc51113a2a60965b18cdee
marcoantonio-dev <maarcoantoniomasson@gmail.com>
2024-04-10 14:23:51 -0300

    criando componente footer e navbar

--- src/App.js
+++ src/App.js
@@ -2,12 +2,15 @@
 import './App.css';
 import Home from "./pages/Home/Home";
 import About from "./pages/About/About";
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 function App() {
   return (
     <div className="App">
       <BrowserRouter>
+      <Navbar />
         <div className="container">
           <Routes>
             <Route path="/" element={<Home />} />
---
+++ src/components/Navbar.js
@@ -0,0 +1,23 @@
+import React from "react";
+import styles from './Navbar.module.css'
+import { NavLink } from "react-router-dom";
+
+const Navbar = () => {
+    return (
+        <nav>
+            <NavLink to="/">
+                BLOG<span>TRAVELS</span>
+            </NavLink>
+            <ul>
+                <li>
+                    <NavLink to="/">Home</NavLink>
+                </li>
+                <li>
+                    <NavLink to="/about">Sobre</NavLink>
+                </li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar
